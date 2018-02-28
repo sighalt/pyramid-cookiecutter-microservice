d3a2638d253fbd54db086bd5d27de5267f7a1936
Jakob Rößler <roessler@sighalt.de>
2018-02-28 10:54:43 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,20 @@
+=================================
+pyramid-cookiecutter-microservice
+=================================
+
+A Cookiecutter for creating a Pyramid microservice project using URL dispatch for routing.
+
+Requirements
+------------
+
+* Python 3
+* `cookiecutter <https://cookiecutter.readthedocs.io/en/latest/installation.html>`_
+
+
+Usage
+-----
+
+.. code-block:: bash
+
+        $ cookiecutter gh:sighalt/pyramid-cookiecutter-microservice
+
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -0,0 +1,15 @@
+from pyramid.config import Configurator
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+    json_renderer = config.registry.getUtility(IRendererFactory, name="json")
+    json_renderer.add_adapter(datetime, lambda o, r: o.isoformat())
+
+    config.add_route("home", "/")
+    config.include("pyramid_json_hyperschema")
+
+    config.scan()
+    return config.make_wsgi_app()
